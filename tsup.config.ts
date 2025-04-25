c6bf4746345b130b0febe429d1ad37d8d1c23275
Thomas Smith <me@spud93.com>
2025-04-25 14:51:56 +0200

    Fixed import issues with tsup.
    Signed-off-by: Thomas Smith <me@spud93.com>

---
+++ tsup.config.ts
@@ -0,0 +1,14 @@
+// tsup.config.ts
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+    entry: ['src/atoms/index.ts'],
+    format: ['esm'],
+    outDir: 'dist/react',
+    dts: {
+        entry: 'src/atoms/index.ts',
+    },
+    clean: true,
+    sourcemap: false,
+    tsconfig: 'tsconfig.react.json',
+});
